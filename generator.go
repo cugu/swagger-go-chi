aa381ad3847d18d01d849b1c4ca5b6bd05bdd6b8
Jonas Plum <git@jonasplum.de>
2022-03-02 13:35:40 +0100

    Add more packages

--- generator.go
+++ generator.go
@@ -4,22 +4,37 @@ import (
 	"bytes"
 	"embed"
 	"fmt"
-	"github.com/rogpeppe/go-internal/modfile"
 	"go/format"
-	"gopkg.in/yaml.v3"
+	"io/fs"
 	"log"
 	"os"
 	"testing/fstest"
 	"text/template"
+
+	"github.com/rogpeppe/go-internal/modfile"
+	"gopkg.in/yaml.v3"
 )
+
+//go:embed api/*
+var api embed.FS
+
+//go:embed time/*
+var time embed.FS
+
+//go:embed pointer/*
+var pointer embed.FS
+
+var packages = []fs.FS{api, time, pointer}
 
 //go:embed templates/*
 var templateFS embed.FS
 
 var generations = []*generation{
-	{"api", "api.go", template.Must(template.New("api.gotmpl").Funcs(funcs).ParseFS(templateFS, "templates/api.gotmpl"))},
+	{"api", "server.go", template.Must(template.New("server.gotmpl").Funcs(funcs).ParseFS(templateFS, "templates/server.gotmpl"))},
 	{"api", "test_api.go", template.Must(template.New("test_api.gotmpl").Funcs(funcs).ParseFS(templateFS, "templates/test_api.gotmpl"))},
 	{"model", "model.go", template.Must(template.New("model.gotmpl").Funcs(funcs).ParseFS(templateFS, "templates/model.gotmpl"))},
+	{"auth", "auth.go", template.Must(template.New("auth.gotmpl").Funcs(funcs).ParseFS(templateFS, "templates/auth.gotmpl"))},
+	{"cli", "cli.go", template.Must(template.New("cli.gotmpl").Funcs(funcs).ParseFS(templateFS, "templates/cli.gotmpl"))},
 }
 
 type generation struct {
@@ -58,6 +73,25 @@ func generate(importPath string, yamlData []byte) (fstest.MapFS, error) {
 			fmtCode = buf.Bytes()
 		}
 		files[templ.Package+"/"+templ.Name] = &fstest.MapFile{Data: fmtCode, Mode: os.ModePerm}
+	}
+
+	for _, fsys := range packages {
+		err := fs.WalkDir(fsys, ".", func(path string, d fs.DirEntry, err error) error {
+			if d.IsDir() {
+				return nil
+			}
+
+			b, err := fs.ReadFile(fsys, path)
+			if err != nil {
+				return err
+			}
+
+			files[path] = &fstest.MapFile{Data: b, Mode: os.ModePerm}
+			return nil
+		})
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	return files, nil
--- go.mod
+++ go.mod
@@ -4,10 +4,12 @@ go 1.17
 
 require (
 	github.com/alecthomas/kong v0.4.1
+	github.com/go-chi/chi v1.5.4
 	github.com/iancoleman/strcase v0.2.0
 	github.com/rogpeppe/go-internal v1.8.1
 	github.com/stretchr/testify v1.7.0
 	github.com/tidwall/sjson v1.2.4
+	github.com/xeipuuv/gojsonschema v1.2.0
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
 )
 
@@ -19,5 +21,7 @@ require (
 	github.com/tidwall/gjson v1.12.1 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.0 // indirect
+	github.com/xeipuuv/gojsonpointer v0.0.0-20180127040702-4e3ac2762d5f // indirect
+	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 )
